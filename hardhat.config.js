12c690d9fd49cd6cf95b5264500d74af1a6b8141
Thomas Cuvillier <titiomm@gmail.com>
2022-06-02 00:23:17 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,42 @@
+require("dotenv").config();
 
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require('hardhat-deploy');
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.4.18",
+      },
+    ],
+  },
+
+  networks: {
+    testnet: {
+      url: "https://testnet.telos.net/evm",
+      accounts: ['0x87ef69a835f8cd0c44ab99b7609a20b2ca7f1c8470af4f0e5b44db927d542084'],
+    },
+  },
+};
