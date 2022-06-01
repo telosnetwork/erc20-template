fc57fc3b96fc44d36ddcf9e72dad44d6a18bf667
Thomas Cuvillier <titiomm@gmail.com>
2022-06-02 00:51:41 +0200

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,10 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-   
+contract Token is ERC20, Ownable {
+    constructor(uint256 _initialSupply, string _name, string _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply * 10 ** decimals());
+    }
 }
