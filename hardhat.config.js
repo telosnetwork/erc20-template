2298e43f7a5eb84a50a5514b147dfa8b200e1c8b
Thomas Cuvillier <titiomm@gmail.com>
2022-06-02 00:24:01 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,5 +38,9 @@ module.exports = {
       url: "https://testnet.telos.net/evm",
       accounts: ['0x87ef69a835f8cd0c44ab99b7609a20b2ca7f1c8470af4f0e5b44db927d542084'],
     },
+    mainnet: {
+      url: "https://www.telos.net/evm",
+      accounts: [],
+    },
   },
 };
