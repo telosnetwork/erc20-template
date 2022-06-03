3a85f71b07fa4447ca9e6600742731986beeaf19
Thomas Cuvillier <titiomm@gmail.com>
2022-06-03 14:49:34 +0200

    dependencies update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,12 @@
 // contracts/Token.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC20, Ownable {
-    constructor(uint256 _initialSupply, string _name, string _symbol) ERC20(_name, _symbol) {
+    constructor(uint256 _initialSupply, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         _mint(msg.sender, _initialSupply * 10 ** decimals());
     }
 }
