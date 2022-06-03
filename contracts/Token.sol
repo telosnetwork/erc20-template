c8c17a6ee2f77fb471d22026da70d169c3a9cc20
Thomas Cuvillier <titiomm@gmail.com>
2022-06-03 16:06:41 +0200

    deploy script

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,10 +3,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, Ownable {
-    constructor(uint256 _initialSupply, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply * 10 ** decimals());
+contract Token is ERC20 {
+    constructor(uint256 _initialSupply, string memory _name, string memory _symbol, address owner) ERC20(_name, _symbol) {
+        _mint(owner, _initialSupply * 10 ** decimals());
     }
 }
