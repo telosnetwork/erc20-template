421311c065c8fc1fd75797268ac5a84a2e351f85
Jesse Schulman <jesse@caleos.io>
2023-09-20 11:17:35 -0700

    Working example of swap function from Office Hours #2

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,4 +8,10 @@ contract Token is ERC20 {
     constructor(uint256 _initialSupply, string memory _name, string memory _symbol, address _owner) ERC20(_name, _symbol) {
         _mint(_owner, _initialSupply * 10 ** decimals());
     }
+
+    function swap() public payable {
+        require(msg.value > 0, "Must swap greater than 0 value");
+        _mint(msg.sender, msg.value);
+    }
+
 }
--- test/Token.js
+++ test/Token.js
@@ -23,6 +23,17 @@ describe("Token contract", function () {
             const balance = await tokenInstance.balanceOf(process.env.ACCOUNT);
             expect(await tokenInstance.totalSupply()).to.equal(balance);
         });
+        it("Swap should be 1:1", async function () {
+            // The sender of test transactions is the first signer from ethers.getSigners(), this is the address which will receive the swap
+            const [addr] = await ethers.getSigners();
+            const oneEther = ethers.utils.parseEther("1.0");
+            await tokenInstance.swap({ value: oneEther });
+            const balance = await tokenInstance.balanceOf(addr.address);
+            expect(balance).to.equal(oneEther);
+        });
+        it("Swap should fail with zero value", async function () {
+            await expect(tokenInstance.swap()).to.be.revertedWith("Must swap greater than 0 value");
+        });
     });
     describe(":: Core", function () {
         it("Should let users approve a contract as spender", async function () {
